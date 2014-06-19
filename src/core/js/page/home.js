7491066608c8b3a2abb2d24d025a99378ebd2586
ningyubo <ningyubo@gmail.com>
2014-06-19 14:38:40 +0800

    init

---
+++ src/core/js/page/home.js
@@ -0,0 +1,19 @@
+(function(){
+	var body, page, header, content, footer, aside, nav, main, base;
+
+
+
+	base = {};
+
+	base.page = {
+
+		init: function(){
+		}
+	}
+
+
+
+
+
+	// base.page.init();
+})();
