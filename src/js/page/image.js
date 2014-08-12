578287e3946199e7f676b54774e1cccb248f4007
gavinning <ningyubo@gmail.com>
2014-08-13 00:19:52 +0800

    优化图片处理模块
    新增上下左右箭头手势，进行图片前后翻页操作

--- src/js/page/image.js
+++ src/js/page/image.js
@@ -10,7 +10,7 @@ var loadImage, LoadImage = require('../lib/loadimage');
 var Page = require('../lib/page');
 var page = new Page;
 
-var dragTargetFolder = "/Volumes/macdata/work/mark/cate";
+var dragTargetFolder = "/Volumes/macdata/work/filter";
 
 
 var selectImg = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAe1BMVEUAAAD///////////////////////////////////////////////////////////////8blfv///+23f5jt/whmPsdlvvK5/5Hqvyu2f4qnPs6pPyj1f2Cxf1bs/z0+v/////j8v9tvPxVsPyTzf13wf3T6v7a7v7r9v8SOWehAAAAEXRSTlMAT2fmjwUnF3TWWJTZvL27CNRr2pIAAAHTSURBVEjHpVZpk4IwDO0qyOGxwysUKIfj7f//hSsFCaVQ1vF9cBhJSPJempQZ8EJnv8YL670TemwBnh9AQ+B7NnMHL/CqLmSSyKKuOF7Yzrm4fmMdi2gAETc+vjtlvwkAnkUGshzYbUz7HyCNo0nEKfAztl8BeRLNQOTAyrAvIwvKtwflc46sOGtZbej7thh95e4OebSIHLs3uz5SsewgUvidvgDxOcNr3fwCrebbpYQy4NEmte0CZFb74glUnaOnKuBWe/noOeSqigDxQq1Ik64WBCojscD/Q76dm5xCe0YV8CwGWoTMQWUjFMBl6O6wPWoLoUe9yWr8sjVURFHLCUJPwFX7A2sGKMuyp4KQ3DHSVAIMUI8pDL0Fh/GV3oHkJFyBkxzFVA6yr09T8AYcs7HqgCpaIdYNLkSoXjTRWgGnon91BG5kSbQOhcuBe9LFHhFKwg1bI0kBLgZPBGoNaj76LsUiUPNp7Z21mV+7agjU3nSAiJuzSSgdIDqixD4pYpxtzxwCJUCaE2gIjMeM4DNjkMYM87Uek3x6jCf9IGNu8L9RGbifDuNPx/0XC+XDlUVLcWZ+1LQUx2s3X1i7Xy325avD95cTuv4c2uvPYer68webMIGKNytpEAAAAABJRU5ErkJggg==';
@@ -31,6 +31,37 @@ page.extend({
 
 		// 进入页面
 		this.enter();
+
+		this.bind();
+	},
+
+	bind: function(){
+
+		window.onkeydown = function(e){
+			var ul, li, current;
+			var index = 0;
+
+			ul = $('.list-images').find('ul')
+			li = ul.find('li')
+
+			// prev
+			if(e.keyCode == 37 || e.keyCode == 38){
+				current = ul.find('li.current')
+				index = li.index(current)
+				if(index > 0){
+					current.removeClass('current').prev().addClass('current')
+				}
+			}
+
+			// next
+			if(e.keyCode == 39 || e.keyCode == 40){
+				current = ul.find('li.current')
+				index = li.index(current)
+				if(index < li.length - 1){
+					current.removeClass('current').next().addClass('current')
+				}
+			}
+		}
 	},
 
 	enter: function(){
